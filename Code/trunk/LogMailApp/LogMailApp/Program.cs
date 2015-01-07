0f3b3909ba006f26b666d55b686b8896647e5fe0
WellerQu <xiaoyao.ning@gmail.com>
2015-01-07 17:03:52 +0800

    Add Source Code.
    Create Project

---
+++ Code/trunk/LogMailApp/LogMailApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogMailApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // 得到 -ui 命令才打开可视窗口
+            App app = new App();
+            app.InitializeComponent();
+            app.MainWindow = new MainWindow();
+            app.Run();
+
+            // 否则执行cmd
+            Cmd cmd = new Cmd();
+            cmd.Execute();
+        }
+    }
+}
